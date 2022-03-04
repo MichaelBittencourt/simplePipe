e353ee9c0bbc5c70509486c18649ed6a758528a9
Michael Bittencourt <mabm@cesar.org.br>
2022-03-04 18:55:53 -0300

    Create a child pipe communication

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #define QTD_MESSAGES 2
+#define READ_PIPE 0
+#define WRITE_PIPE 1
 
 int main() {
     int pipeFileDescriptors[2];
@@ -18,12 +23,29 @@ int main() {
         return 1;
     }
 
-    for (int i = 0; i < QTD_MESSAGES; i++) {
-        printf("Writing Message %d is %s\n", i, writeMessages[i]);
-        write(pipeFileDescriptors[1], writeMessages[0], sizeof(char) * 20);
-        read(pipeFileDescriptors[0], readMessages, sizeof(char) * 20);
-        printf("Reading Message %d is %s\n", i, readMessages);
+    pid_t processId = fork();
+
+    if (processId == 0) {
+        srand(time(NULL));
+        close(pipeFileDescriptors[READ_PIPE]);
+
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            printf("Writing Message %d is %s\n", i, writeMessages[i]);
+            write(pipeFileDescriptors[WRITE_PIPE], writeMessages[i],
+                  sizeof(char) * 20);
+            sleep(rand() % 20 / 10.0);
+        }
+    } else {
+        close(pipeFileDescriptors[WRITE_PIPE]);
+
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            read(pipeFileDescriptors[READ_PIPE], readMessages,
+                 sizeof(char) * 20);
+            printf("Reading Message %d is %s\n", i, readMessages);
+        }
     }
+
+    wait(NULL);
 
     return 0;
 }

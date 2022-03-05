589ef03d521d96c9f3e3fb30789bf1059f910d4f
Michael Bittencourt <mabm@cesar.org.br>
2022-03-05 13:39:59 -0300

    Add two pipes. One to send message from parent to child and other from child to parent

--- main.c
+++ main.c
@@ -10,38 +10,54 @@
 #define WRITE_PIPE 1
 
 int main() {
-    int pipeFileDescriptors[2];
+    int pipeParent[2];
+    int pipeChild[2];
     int returnStatus;
-    char writeMessages[QTD_MESSAGES][20] = {"Maria", "Joao andou a noite"};
+    char writeParent[QTD_MESSAGES][20] = {"Maria", "Joao andou a noite"};
+    char writeChild[QTD_MESSAGES][20] = {"Sou o filho", "Filho na chuva"};
     char readMessages[20];
     memset(readMessages, '\0', sizeof(char) * 20);
 
-    returnStatus = pipe(pipeFileDescriptors);
+    returnStatus = pipe(pipeParent);
 
     if (returnStatus == -1) {
         printf("Error when create pipe\n");
         return 1;
     }
 
+    returnStatus = pipe(pipeChild);
+
     pid_t processId = fork();
 
     if (processId == 0) {
         srand(time(NULL));
-        close(pipeFileDescriptors[READ_PIPE]);
+        close(pipeParent[READ_PIPE]);
+        close(pipeChild[WRITE_PIPE]);
 
         for (int i = 0; i < QTD_MESSAGES; i++) {
-            printf("Writing Message %d is %s\n", i, writeMessages[i]);
-            write(pipeFileDescriptors[WRITE_PIPE], writeMessages[i],
-                  sizeof(char) * 20);
+            printf("Writing from parent: Message %d is %s\n", i,
+                   writeParent[i]);
+            write(pipeParent[WRITE_PIPE], writeParent[i], sizeof(char) * 20);
             sleep(rand() % 20 / 10.0);
         }
-    } else {
-        close(pipeFileDescriptors[WRITE_PIPE]);
 
         for (int i = 0; i < QTD_MESSAGES; i++) {
-            read(pipeFileDescriptors[READ_PIPE], readMessages,
-                 sizeof(char) * 20);
-            printf("Reading Message %d is %s\n", i, readMessages);
+            read(pipeChild[READ_PIPE], readMessages, sizeof(char) * 20);
+            printf("Reading from parent Message %d is %s\n", i, readMessages);
+        }
+    } else {
+        close(pipeParent[WRITE_PIPE]);
+        close(pipeChild[READ_PIPE]);
+
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            read(pipeParent[READ_PIPE], readMessages, sizeof(char) * 20);
+            printf("Reading from child Message %d is %s\n", i, readMessages);
+        }
+
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            printf("Writing from child: Message %d is %s\n", i, writeChild[i]);
+            write(pipeChild[WRITE_PIPE], writeChild[i], sizeof(char) * 20);
+            sleep(rand() % 20 / 10.0);
         }
     }
 
